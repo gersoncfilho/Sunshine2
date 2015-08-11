74e12fc035d2c8d98e1b32cdc4b0d2b9095dc100
Gerson Cardoso Filho <gersoncfilho@mac.com>
2015-08-11 16:55:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/app/connection/MyAsynkMethods.java
@@ -0,0 +1,5 @@
+package com.example.android.sunshine.app.connection;
+
+public interface MyAsynkMethods{
+    public void onPostExecute(String result);
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/transaction/ForecastTransaction.java
@@ -0,0 +1,13 @@
+package com.example.android.sunshine.app.transaction;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by gersoncardoso on 11/08/2015.
+ */
+public class ForecastTransaction extends AsyncTask<String, void, String> {
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
